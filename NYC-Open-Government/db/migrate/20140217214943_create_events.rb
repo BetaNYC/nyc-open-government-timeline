78bbaed9e8a5d276be836b16b98f26fb9b25958b
MsUzoAgu <uzolearns@gmail.com>
2014-02-17 16:56:41 -0500

    generate model Comment

--- NYC-Open-Government/db/migrate/20140217214943_create_events.rb
+++ NYC-Open-Government/db/migrate/20140217214943_create_events.rb
@@ -1,6 +1,12 @@
 class CreateEvents < ActiveRecord::Migration
   def change
     create_table :events do |t|
+      t.string :name
+      t.text :description
+      t.string :type
+      t.datetime :date 
+      t.string :url 
+      t.string :status
 
       t.timestamps
     end
