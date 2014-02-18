0ecb9979f9d94b9d4f8a8e755f7cd1540171ab2c
Katie Hoffman <ktahoffman@gmail.com>
2014-02-18 10:16:42 -0500

    adds actions to comments_controller

--- NYC-Open-Government/app/controllers/comments_controller.rb
+++ NYC-Open-Government/app/controllers/comments_controller.rb
@@ -1,4 +1,48 @@
 class CommentsController < ApplicationController
 
-  
+  def show
+    @comment = Comment.find(params[:id])
+    @event = Event.find(params[:event_id])
+  end
+
+  def new
+    @comment = Comment.new
+    @event = Event.find(params[:event_id])
+  end
+
+  def create
+    @event = Event.find(params[:event_id])
+    @comment = Comment.new(params_comment)
+    @comment.event_id = params[:event_id]
+    if @comment.save
+      redirect_to events_path
+    else
+      render "new"
+    end
+  end
+
+  def edit
+    @comment = Comment.find(params[:id])
+    @event = Event.find(params[:event_id])
+  end
+
+  def update
+    @comment = Comment.find(params[:id])
+    @event = Event.find(params[:event_id])
+    if @comment.update_attribute(params_comment)
+      redirect_to events_path
+    else
+      render "new"
+    end
+  end
+
+  def destroy
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+  end
+
+  private
+    def params_comment
+      params.require(:comment).permit(:content, :author)
+    end
 end
