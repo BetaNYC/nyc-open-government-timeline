3251a078f24ff8cca5c5137ae00b0d681a0f0f9f
Oliver Switzer <oliverswitzer@gmail.com>
2014-02-20 11:02:04 -0500

    update list for attributes of events in show view

--- NYC-Open-Government/app/models/category.rb
+++ NYC-Open-Government/app/models/category.rb
@@ -4,5 +4,5 @@ class Category < ActiveRecord::Base
   attr_accessible :name
 
   has_many :category_events
-  has_many :events, :through => :category_events 
+  has_many :events, :through => :category_events
 end
