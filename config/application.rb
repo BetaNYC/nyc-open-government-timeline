378e161899bb42020aa7a40c270dc0663e3609f6
Katie Hoffman <ktahoffman@gmail.com>
2014-03-03 10:22:41 -0500

    sets config assets to false

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,7 @@ Bundler.require(:default, Rails.env)
 
 module NYCOpenGovernment
     class Application < Rails::Application
+        config.assets.initialize_on_precompile = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
