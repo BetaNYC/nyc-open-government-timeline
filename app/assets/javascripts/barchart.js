4b60e097de9438b0d8b4e490188a5a52e2283eab
Katie Hoffman <ktahoffman@gmail.com>
2014-03-04 19:06:31 -0500

    renders large blue rectangle'

--- app/assets/javascripts/barchart.js
+++ app/assets/javascripts/barchart.js
@@ -13,7 +13,6 @@ d3.json('/events.json', function (events) {
   var xMax = sortedByYear[(sortedByYear.length - 1)][0];
   //get the difference between min and max years
   var range = xMax - xMin;
-  //sets y min to zero
   //sorts arrays by number of events
   var sortedByValue = sortArraysByValue(sortedByYear);
   //sets width and height of barchart
@@ -23,71 +22,60 @@ d3.json('/events.json', function (events) {
 
   //add zero values to years not present in preDataHash
   var dataHash = addMissingYears(preDataHash, xMin, xMax);
- 
+
   //makes json objects into nested array with year and count of events
-  var oneLastThingData = makeTwoDArray(dataHash);
-  // make array with just the values of each year
+  var allValueArray = makeTwoDArray(dataHash);
+  // allValueArray = [["1974",2],["1975",0],["1976",0],["1977",0],["1978",0]...]
 
-  var domain = flattenArray(oneLastThingData, 0);
-  var range = flattenArray(oneLastThingData, 1);
-  // domain = ["1974", "1975", "1976", "1977", "1978", "1979", "1980", "1981", "1982", "1983", "1984", "1985", "1986", "1987", "1988", "1989", "1990", "1991", "1992", "1993", "1994", "1995", "1996", "1997", "1998", "1999", "2000", "2001", "2002", "2003", "2004", "2005", "2006", "2007", "2008", "2009", "2010", "2011", "2012", "2013"]
-  // range = [2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 1, 3, 2, 0, 1, 2, 0, 1, 11, 4, 4, 10, 21] 
+  var xRange = flattenArray(allValueArray, 0);
+  var yRange = flattenArray(allValueArray, 1);
+  // xRange = ["1974", "1975", "1976", "1977", "1978", "1979", "1980", "1981", "1982", "1983", "1984", "1985", "1986", "1987", "1988", "1989", "1990", "1991", "1992", "1993", "1994", "1995", "1996", "1997", "1998", "1999", "2000", "2001", "2002", "2003", "2004", "2005", "2006", "2007", "2008", "2009", "2010", "2011", "2012", "2013"]
+  // yRange = [2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 1, 3, 2, 0, 1, 2, 0, 1, 11, 4, 4, 10, 21] 
 
-  var data = d3.scale.ordinal()
-    .domain(domain)
-    .range(range);
+  /////// SVG D3 Begins ///////////////////////////////////////////
 
-  // SVG D3 Begins ###################################################################################
+  var data = yRange;
 
-  var margin = {top: 20, right: 30, bottom: 30, left: 40},
-      width = 960 - margin.left - margin.right,
-      height = 500 - margin.top - margin.bottom;
+  var width = 800,
+    height = 600;
 
   var x = d3.scale.ordinal()
-      .rangeRoundBands([0, width], .1);
+    .rangeRoundBands([0, width], .1);
 
   var y = d3.scale.linear()
-      .range([height, 0]);
+    .range([height, 0]);
 
   var xAxis = d3.svg.axis()
-      .scale(x)
-      .orient("bottom");
+    .scale(x)
+    .orient("bottom");
 
   var yAxis = d3.svg.axis()
-      .scale(y)
-      .orient("left");
+    .scale(y)
+    .orient("left");
 
   var chart = d3.select(".barchart")
-      .attr("width", width + margin.left + margin.right)
-      .attr("height", height + margin.top + margin.bottom)
-      .append("g")
-      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    .attr("width", width)
+    .attr("height", height);
 
-  chart.append("g")
-      .attr("class", "x axis")
-      .attr("transform", "translate(0," + height + ")")
-      .call(xAxis);
+  var barWidth = width / data.length;
 
-  chart.append("g")
-      .attr("class", "y axis")
-      .call(yAxis);
+  var bar = chart.selectAll("g")
+    .data(data)
+    .enter().append("g")
+    .attr("transform", function (d, i) {
+      return "translate(" + i * barWidth + ",0)";
+    });
 
-  chart.selectAll(".bar")
-      .data(data)
-    .enter().append("rect")
-      .attr("class", "bar")
-      .attr("x", function(d) { return x(d.name); })
-      .attr("y", function(d) { return y(d.value); })
-      .attr("height", function(d) { return height - y(d.value); })
-      .attr("width", x.rangeBand());
+  bar.append("rect")
+    .attr("y", function (d) {
+      return y(d);
+    })
+    .attr("height", function (d) {
+      return height - y(d);
+    })
+    .attr("width", x.rangeBand());
 
-  function type(d) {
-    d.value = +d.value; // coerce to number
-    return d;
-}
-
-
- // Functions for data begin ###################################################################################
+  ////// SVG / D3 End, Functions for getting data begin ///////////////////////////////
 
   function sortArraysByYear(arrays) {
     arrays.sort(function (a, b) {
