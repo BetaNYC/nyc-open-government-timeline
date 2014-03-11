bd8712db0cdfe2358cb9ecc8fd7301dc158d02c5
Oliver Switzer <oliverswitzer@gmail.com>
2014-03-11 11:15:14 -0400

    add view for timelineJS and a custom javascript for it timelineJS.js

---
+++ app/assets/javascripts/timelineJS.js
@@ -0,0 +1,9 @@
+ $(document).ready(function() {
+  createStoryJS({
+      type:       'timeline',
+      width:      '800',
+      height:     '600',
+      source:     'path_to_json/or_link_to_googlespreadsheet',
+      embed_id:   'my-timeline'
+  });
+});
