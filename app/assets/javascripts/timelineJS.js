1d076a66f31b0df96f9c9962a3fd16b5ee4d3477
Katie Hoffman <ktahoffman@gmail.com>
2014-03-12 12:49:22 -0400

    adds css to jquery instead of putting it in a style tag

--- app/assets/javascripts/timelineJS.js
+++ app/assets/javascripts/timelineJS.js
@@ -7,5 +7,5 @@
       embed_id:   'my-timeline'
   });
 
-  $(".timenav-tag-size-half").children().children().addClass("darkenify");
+  $(".timenav-tag-size-half").children().children().css('color':'black');;
 });
