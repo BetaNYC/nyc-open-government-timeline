8a16a1c509569ff0c01a3b91e88137639a0d9ac2
Chris <chris.haack@me.com>
2014-03-16 12:05:24 -0400

    set sign_out in devise.rb to get - note to change back to delete request with destroy_user_session path when root view is functioning

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+
+  attr_accessible :email, :password
+
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :lockable
 end
