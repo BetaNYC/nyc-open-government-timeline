0641ff6358cbb69835460757f2c5435d238b657c
Chris <chris.haack@me.com>
2014-03-17 19:48:01 -0400

    add conditionals to check for admin in event views, delete link to sign up in _links.erb

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
 
-  attr_accessible :email, :password
+  attr_accessible :email, :password, :admin
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :lockable
